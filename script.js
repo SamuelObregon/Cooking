1c2d02e1bbd09874756ec744b460e909ab570045
Samuel Obregon <samuelobregon@outlook.com>
2016-05-26 21:46:05 +0000

    Update

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var FINISH_IMG = "images/finish.png";
 var TEXT_COLOR = "purple";
 
 // Stores the hero sprite position.
-//var salmon = {
-//  speed: 50, // Movement speed in pixels per second.
-// x: 0,
-// y: 0
-//};
+var salmon = {
+  speed: 50, // Movement speed in pixels per second.
+ x: 0,
+ y: 0
+};
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -20,16 +20,16 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-  //  var data = ev.dataTransfer.getData("text");
-//    ev.target.appendChild(document.getElementById(data));
-  //  if (hero.x > finish.x && hero.x < finish.x + finish.width &&
-  //      hero.y > finish.y && hero.y < finish.y + finish.height)
-    //    if (false) {
-     //       wins++;
-       //     canvas.fillText("YOU WON!", 50, 100);
-         //   reset();
-        }
-    var myElement = document.querySelector("knife"); 
-    var position = getPosition(myElement);
-    alert("The image is located at: " + position.x + ", " + position.y);
-    
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));}
+      //    if (hero.x > finish.x && hero.x < finish.x + finish.width &&
+        // hero.y > finish.y && hero.y < finish.y + finish.height)
+          // if (false) {
+            //  wins++;
+            //canvas.fillText("YOU WON!", 50, 100);
+          //reset();
+        //}
+    //var myElement = document.querySelector("knife"); 
+    //var position = GetPosition(myElement);
+    //alert("The image is located at: " + position.x + ", " + position.y);
+//}
